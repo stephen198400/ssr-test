183a8e98da1bc06d7939281d357f3cb89b72ca93
Cicada <89372746+stephen198400@users.noreply.github.com>
2025-04-13 21:50:50 +0800

    移除服务器配置选项，进一步简化应用程序配置

--- app.config.ts
+++ app.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from '@tanstack/react-start/config';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-	server: {
-		preset: 'vercel',
-	},
 	tsr: {
 		appDirectory: 'src',
 	},
