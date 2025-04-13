23815ce9456aec1fe717f75e07f7ce04275f9577
Cicada <89372746+stephen198400@users.noreply.github.com>
2025-04-13 21:13:13 +0800

    更新首页路由，重定向至仪表板组件

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,9 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Navigate } from '@tanstack/react-router';
 
 export const Route = createFileRoute('/')({
-  component: RouteComponent,
-})
+	component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>
+	return <Navigate to="/dashboard" />;
 }
